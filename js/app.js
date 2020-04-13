47d99f282df7e4ea7928775263a63fcbab95ecb9
Pinter Robert <pinterr8833@gmail.com>
2020-04-13 14:25:38 +0200

    Added menu and active section functions
    addactiveclass adds the calss to the main sections
    
    buildnav() constructing the navigation menu
    
    scrollposactive() watching for th active position on the screen to 
    toggle the class.

--- js/app.js
+++ js/app.js
@@ -1,43 +1,82 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
 /**
  * Define Global Variables
- * 
+ *
 */
-
+const content = document.getElementsByClassName('landing__container');
+const navBar = document.getElementById('navbar__list');
+window.addEventListener('scroll', scrollPosActive);
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+ *
 */
-
-
-
+function addActiveClass(){
+  for (section of content){
+    const myClass = section.parentElement.classList
+    if(myClass.contains('your-active-class')){
+      myClass.toggle('your-active-class');
+    }
+    else{
+      myClass.add('your-active-class');
+      myClass.toggle('your-active-class');
+    }
+  }
+}
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
 
 // build the nav
+function buildNav(){
+  const navItems = document.createDocumentFragment();
 
+  navBar.innerHTML = "";
+
+  for (let section of content){
+    const newNavItem = document.createElement('li')
+    const navText = section.parentElement.dataset.nav;
+    const navId = section.parentElement.id;
+
+    newNavItem.innerHTML = `<a class='menu__link' href='#${navId}'>${navText}</a>`;
+
+    navItems.appendChild(newNavItem);
+  }
+  navBar.appendChild(navItems);
+}
 
 // Add class 'active' to section when near top of viewport
-
+function scrollPosActive(){
+  for (section of content){
+    const sectionYPos = section.getBoundingClientRect().top;
+    const sectionHeight = section.getBoundingClientRect().height;
+    const myClass = section.parentElement.classList;
+    if (sectionYPos < 100 &&
+        sectionYPos >= -sectionHeight){
+      myClass.toggle('your-active-class');
+    }
+    else{
+      myClass.toggle('your-active-class');
+    }
+  }
+}
 
 // Scroll to anchor ID using scrollTO event
 
@@ -45,13 +84,12 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
-
-// Build menu 
-
+//Add your-active-class to the section in <main> tag
+addActiveClass();
+// Build menu
+buildNav();
 // Scroll to section on link click
 
 // Set sections as active
-
-
